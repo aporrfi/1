95a81f86134767184623d4a55c980c79a3999f87
aporrfi <max090803@gmail.com>
2023-04-20 04:16:28 +1200

    3

--- test.py
+++ test.py
@@ -4,6 +4,8 @@ def get_size(list_of_values):
     n = int(math.sqrt(len(list_of_values)))
     return n
 
+
+
 def draw_row(data_list):
     str_i = "│"
     for data in data_list:
@@ -14,6 +16,7 @@ def draw_row(data_list):
         elif len(data) == 2:
             str_i += "-" + data + "-│"
     return str_i
+
 
 
 def draw_grid(n):
@@ -37,6 +40,7 @@ def draw_grid(n):
     print(grid_str)
 
 
+
 def get_input(list_of_valid_choices):
     while True:
         choice = input("Enter a value: ")
@@ -47,8 +51,38 @@ def get_input(list_of_valid_choices):
         else: 
             print(end="")
 
+
+
 def is_vertically_adjacent(n, first, second):
     return abs(first - second) == n
 
+
+
 def is_horizontally_adjacent(n, first, second):
     return first // n == second // n and abs(first - second) == 1
+
+
+
+def swap_tile(list_of_tiles, tile_choice):
+        empty_index = list_of_tiles.index('')
+        tile_index = list_of_tiles.index(tile_choice)
+        list_of_tiles[empty_index], list_of_tiles[tile_index] = \
+            list_of_tiles[tile_index], list_of_tiles[empty_index]
+
+
+
+def is_complete(list_of_tiles):
+    n = int(math.sqrt(len(list_of_tiles)))
+
+    if "" not in list_of_tiles:
+        return False
+
+    nums = []
+    for tile in list_of_tiles:
+        if tile == "":
+            nums.append(None)
+        else:
+            nums.append(int(tile))
+
+    sorted_nums = sorted([num for num in nums if num is not None])
+    return nums[:len(sorted_nums)] == sorted_nums and len(nums) == n * n
--- test2.py
+++ test2.py
@@ -1,3 +1,17 @@
-data = input("Enter assignment data: ")
+import math
+def is_complete(list_of_tiles):
+    n = int(math.sqrt(len(list_of_tiles)))
 
-data_list = data.split(",")
+    if "" not in list_of_tiles:
+        return False
+
+    nums = []
+    for tile in list_of_tiles:
+        if tile == "":
+            nums.append(None)
+        else:
+            nums.append(int(tile))
+
+    sorted_nums = sorted([num for num in nums if num is not None])
+    return nums[:len(sorted_nums)] == sorted_nums and len(nums) == n * n
+print(is_complete(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '15', '14', '']))
